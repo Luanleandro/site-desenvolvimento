488aa42924271851ff5cbb3bd102fcbf5ed7c18b
Luan Leandro <luan.barbosacruz@gmail.com>
2022-08-12 12:18:07 -0300

    adicionando responsividade

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const btnMobile = document.querySelector("#btn-mobile");
 const featureBtn = document.querySelector("#container-feature");
 const companyBtn = document.querySelector("#container-company");
+const body = document.querySelector("#body")
 
 const handleOnCLick = (event) => {
   event.preventDefault();
   const nav = document.querySelector("#nav");
   nav.classList.toggle("active");
+  body.classList.add("body-active")
+
 };
 
 const handleClickFeature = () => {
